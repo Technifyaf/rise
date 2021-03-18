62793df3124b0d7f70a0de26920b1afbc918d6e3
Technifyaf <Technifyaf@gmail.com>
2021-03-18 11:46:06 +0200

    hide

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function IndexPage() {
             />
             <Banner />
           
-            <CtaTwo/>
+           
             <CtaThree/>
             <FAQ/>
          
